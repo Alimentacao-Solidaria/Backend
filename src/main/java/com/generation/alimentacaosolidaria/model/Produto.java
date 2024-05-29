b77725a2a62f66adb69156da8e811787041a92fe
Mayara Rocha <mayara_rocha.pej@hotmail.com>
2024-05-29 10:25:55 -0300

    task 4: implementando model de produto e usuario

---
+++ src/main/java/com/generation/alimentacaosolidaria/model/Produto.java
@@ -0,0 +1,80 @@
+package com.generation.alimentacaosolidaria.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome do produto é obrigatório")
+	@Size(min = 4, max = 50, message = "O campo nome do produto precisa ter entre 4 e 50 caracteres.")
+	private String nomeProduto;
+	
+	@NotBlank(message = "O atributo descrição do produto é obrigatório")
+	@Size(min = 4, max = 255, message = "O campo descrição do produto precisa ter entre 4 e 255 caracteres.")
+	private String descricaoProduto;
+	
+	@NotNull
+	private float preco;
+	
+	@NotNull
+	private int quantidade;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+
+	public String getDescricaoProduto() {
+		return descricaoProduto;
+	}
+
+	public void setDescricaoProduto(String descricaoProduto) {
+		this.descricaoProduto = descricaoProduto;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+}
