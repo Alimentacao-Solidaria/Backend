9eb0843ed2ef1108351cde6a23d4d4a8998e0162
GiovanaRods <gigi.hinata13@gmail.com>
2024-05-28 23:35:57 -0300

    adicionando erro no delete

--- src/main/java/com/generation/alimentacaosolidaria/controller/CategoriaController.java
+++ src/main/java/com/generation/alimentacaosolidaria/controller/CategoriaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -67,6 +68,7 @@ public class CategoriaController {
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
 	@DeleteMapping("/{id}")
 	public void delete(@PathVariable Long id){
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
