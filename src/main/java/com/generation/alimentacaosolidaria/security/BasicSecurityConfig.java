f85beeda4a5412b61f6b1140c4aaf7a928f10341
GiovanaRods <gigi.hinata13@gmail.com>
2024-06-05 17:36:09 -0300

    security

--- src/main/java/com/generation/alimentacaosolidaria/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/alimentacaosolidaria/security/BasicSecurityConfig.java
@@ -62,7 +62,7 @@ public class BasicSecurityConfig {
         .authorizeHttpRequests((auth) -> auth
                 .requestMatchers("/usuarios/logar").permitAll()
                 .requestMatchers("/usuarios/cadastrar").permitAll()
-                .requestMatchers("/produtos").permitAll()
+                .requestMatchers("/produtos/todosprodutos").permitAll()
                 .requestMatchers("/produtos/{id}").permitAll()
                 .requestMatchers("/produtos/nome/{nomeProduto}").permitAll()
                 .requestMatchers("/categoria").permitAll()
